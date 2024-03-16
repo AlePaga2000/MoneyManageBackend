01900abbb436431f6fc264aa47ef63f7f426f0eb
RR2000 <r2000raoul@gmail.com>
2024-03-16 23:11:01 +0100

    updated gradle.yml

--- src/test/java/com/rondinella/moneymanageapi/MoneyManageApiApplicationTests.java
+++ src/test/java/com/rondinella/moneymanageapi/MoneyManageApiApplicationTests.java
@@ -2,12 +2,18 @@ package com.rondinella.moneymanageapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootTest
 class MoneyManageApiApplicationTests {
 
   @Test
   void contextLoads() {
+    // Your test code here
   }
 
+  @Configuration // This class serves as a test configuration
+  static class TestConfiguration {
+    // Define necessary configurations for your tests here
+  }
 }
