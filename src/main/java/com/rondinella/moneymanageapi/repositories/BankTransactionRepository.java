efccaa8c28a30ab120998f533ec74327ea64a129
RR2000 <r2000raoul@gmail.com>
2024-03-27 08:38:48 +0100

    Hide repos from swagger

--- src/main/java/com/rondinella/moneymanageapi/repositories/BankTransactionRepository.java
+++ src/main/java/com/rondinella/moneymanageapi/repositories/BankTransactionRepository.java
@@ -1,12 +1,14 @@
 package com.rondinella.moneymanageapi.repositories;
 
 import com.rondinella.moneymanageapi.enitities.BankTransaction;
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.sql.Timestamp;
 import java.util.List;
 
+@Hidden
 public interface BankTransactionRepository extends JpaRepository<BankTransaction, Timestamp> {
   @Query("SELECT DISTINCT t.account FROM BankTransaction t")
   List<String> findDistinctAccounts();
--- src/main/java/com/rondinella/moneymanageapi/repositories/BrokerTransactionRepository.java
+++ src/main/java/com/rondinella/moneymanageapi/repositories/BrokerTransactionRepository.java
@@ -2,7 +2,9 @@ package com.rondinella.moneymanageapi.repositories;
 
 import com.rondinella.moneymanageapi.enitities.BrokerTransaction;
 import com.rondinella.moneymanageapi.enitities.ids.BrokerTransactionId;
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Hidden
 public interface BrokerTransactionRepository extends JpaRepository<BrokerTransaction, BrokerTransactionId> {
 }
